7de0b6ca332c42eadb86e288453e08caca135e94
quockhanh04 <22520637@gm.uit.edu.vn>
2024-02-02 13:12:06 +0700

    update icons for tiktok

--- src/components/Icons/Icon.js
+++ src/components/Icons/Icon.js
@@ -12,6 +12,13 @@ const {
   faLightbulb,
   faGear,
   faLanguage,
+  faQuestion,
+  faCircleQuestion,
+  faKeyboard,
+  faMoon,
+  faRightFromBracket,
+  faUserGroup,
+  faCompass,
 } = require("@fortawesome/free-solid-svg-icons");
 const { FontAwesomeIcon } = require("@fortawesome/react-fontawesome");
 
@@ -28,3 +35,9 @@ export const cameraIcon = <FontAwesomeIcon icon={faCamera} />;
 export const lightBubIcon = <FontAwesomeIcon icon={faLightbulb} />;
 export const gearIcon = <FontAwesomeIcon icon={faGear} />;
 export const languageIcon = <FontAwesomeIcon icon={faLanguage} />;
+export const questionIcon = <FontAwesomeIcon icon={faCircleQuestion} />;
+export const keyboardIcon = <FontAwesomeIcon icon={faKeyboard} />;
+export const moonIcon = <FontAwesomeIcon icon={faMoon} />;
+export const bracketIcon = <FontAwesomeIcon icon={faRightFromBracket} />;
+export const userGroupIcon = <FontAwesomeIcon icon={faUserGroup} />;
+export const compassIcon = <FontAwesomeIcon icon={faCompass} />;
