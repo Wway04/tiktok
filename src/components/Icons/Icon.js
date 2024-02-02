24b5722cc1869121c593b4006e14806b00cf710f
quockhanh04 <22520637@gm.uit.edu.vn>
2024-02-02 12:43:49 +0700

    create icons for tiktok

---
+++ src/components/Icons/Icon.js
@@ -0,0 +1,30 @@
+const {
+  faSearch,
+  faCircleXmark,
+  faSpinner,
+  faPlus,
+  faPaperPlane,
+  faMessage,
+  faUser,
+  faBookmark,
+  faCircleDollarToSlot,
+  faCamera,
+  faLightbulb,
+  faGear,
+  faLanguage,
+} = require("@fortawesome/free-solid-svg-icons");
+const { FontAwesomeIcon } = require("@fortawesome/react-fontawesome");
+
+export const searchIcon = <FontAwesomeIcon icon={faSearch} />;
+export const deleteIcon = <FontAwesomeIcon icon={faCircleXmark} />;
+export const loadingIcon = <FontAwesomeIcon icon={faSpinner} />;
+export const plusIcon = <FontAwesomeIcon icon={faPlus} />;
+export const paperPlaneIcon = <FontAwesomeIcon icon={faPaperPlane} />;
+export const messageIcon = <FontAwesomeIcon icon={faMessage} />;
+export const userIcon = <FontAwesomeIcon icon={faUser} />;
+export const bookMarkIcon = <FontAwesomeIcon icon={faBookmark} />;
+export const coinIcon = <FontAwesomeIcon icon={faCircleDollarToSlot} />;
+export const cameraIcon = <FontAwesomeIcon icon={faCamera} />;
+export const lightBubIcon = <FontAwesomeIcon icon={faLightbulb} />;
+export const gearIcon = <FontAwesomeIcon icon={faGear} />;
+export const languageIcon = <FontAwesomeIcon icon={faLanguage} />;
